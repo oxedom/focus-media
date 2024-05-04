7d6d3e73a2f8edce397f6d140644e78737551937
Sam <oxedom@gmail.com>
2024-05-04 20:57:19 +0300

    fix: poc wip

---
+++ content.js
@@ -0,0 +1,39 @@
+let body = document.querySelector("body");
+
+const elementsNotToPaint = [
+  "IMG",
+  "STYLE",
+  "VIDEO",
+  "SCRIPT",
+  "META",
+  "LINK",
+  "CANVAS",
+  "INPUT",
+  "BUTTON",
+  "SELECT",
+  "TEXTAREA",
+  "SVG",
+];
+
+const paintNonImages = (element) => {
+  let start = performance.now();
+  for (const child of element.children) {
+    const currentTag = child.tagName;
+    if (elementsNotToPaint.includes(currentTag)) {
+      continue;
+    }
+
+    if (child.children.length > 0) {
+      paintNonImages(child);
+    }
+
+    if (child.children.length === 0 && child.style) {
+      child.style.filter = "grayscale(100%)";
+    }
+  }
+  let end = performance.now();
+  console.log("Time taken: ", end - start);
+};
+
+// document.addEventListener("DOMContentLoaded", () => paintNonImages(body));
+paintNonImages(body);
